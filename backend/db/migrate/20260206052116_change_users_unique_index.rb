362693423f989146719b9c43f9f2b4809fcb3e6e
Ameeen-Khan <ameen.khan@joshsoftware.com>
2026-02-06 11:13:20 +0530

    fix: add partial index to handle uniqueness after deletion

---
+++ backend/db/migrate/20260206052116_change_users_unique_index.rb
@@ -0,0 +1,14 @@
+class ChangeUsersUniqueIndex < ActiveRecord::Migration[7.1]
+  def change
+    # Remove old global unique index
+    remove_index :users,
+                 name: "index_users_on_email"
+
+    # Add soft delete aware unique index
+    add_index :users,
+              :email,
+              unique: true,
+              where: "deleted_at IS NULL",
+              name: "index_users_on_email_active_unique"
+  end
+end
