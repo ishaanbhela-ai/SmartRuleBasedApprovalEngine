b8a318f9c2af35d916ce41dc2dc92727c5599cfc
Ameeen-Khan <ameen.khan@joshsoftware.com>
2026-02-06 15:55:19 +0530

    fix: user and rule creation after soft delete

--- backend/app/models/rule.rb
+++ backend/app/models/rule.rb
@@ -5,5 +5,5 @@ class Rule < ApplicationRecord
 
   validates :grade, inclusion: { in: [ 1, 2, 3 ] }
   validates :definition, presence: true, numericality: { greater_than: 0 }
-  validates :request_type_id, uniqueness: { scope: :grade }
+  validates :request_type_id, uniqueness: { scope: :grade, conditions: -> { where(deleted_at: nil) } }
 end
